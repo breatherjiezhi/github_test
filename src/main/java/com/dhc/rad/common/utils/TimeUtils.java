c3f0c84ad0f7d7d12bae455477c0b3657877def3
zhengxiang <1095134226@qq.com>
2021-04-28 16:46:21 +0800

    添加日期工具类

--- src/main/java/com/dhc/rad/common/utils/TimeUtils.java
+++ src/main/java/com/dhc/rad/common/utils/TimeUtils.java
@@ -3,40 +3,84 @@
  */
 package com.dhc.rad.common.utils;
 
-import java.util.Arrays;
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 /**
  * 时间计算工具类
+ *
  * @author DHC
  * @version 2013-11-03
  */
 public class TimeUtils {
-	
-	public static String toTimeString(long time) {
-		TimeUtils t = new TimeUtils(time);
-		int day = t.get(TimeUtils.DAY);
-		int hour = t.get(TimeUtils.HOUR);
-		int minute = t.get(TimeUtils.MINUTE);
-		int second = t.get(TimeUtils.SECOND);
-		StringBuilder sb = new StringBuilder();
-		if (day > 0){
-			sb.append(day).append("天");
-		}
-		if (hour > 0){
-			sb.append(hour).append("时");
-		}
-		if (minute > 0){
-			sb.append(minute).append("分");
-		}
-		if (second > 0){
-			sb.append(second).append("秒");
-		}
-		return sb.toString();
-	}
-	
+
+    /**
+     * @Description: 获取当前日期的下周一到下周日的所有日期集合
+     * @Param: null
+     * @return:  List<String>
+     * @Author: zhengXiang
+     * @Date: 2021/4/28
+     */
+    public static List<String> getNextWeekDateList() {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+
+        cal1.setTime(new Date());
+
+        cal2.setTime(new Date());
+        // 获得当前日期是一个星期的第几天
+        int dayWeek = cal1.get(Calendar.DAY_OF_WEEK);
+
+        if (dayWeek == 1) {
+            cal1.add(Calendar.DAY_OF_MONTH, 1);
+            cal2.add(Calendar.DAY_OF_MONTH, 7);
+        } else {
+            cal1.add(Calendar.DAY_OF_MONTH, 1 - dayWeek + 8);
+            cal2.add(Calendar.DAY_OF_MONTH, 1 - dayWeek + 14);
+        }
+        Calendar cStart = Calendar.getInstance();
+        cStart.setTime(cal1.getTime());
+
+        List<String> dateList = new ArrayList();
+        //别忘了，把起始日期加上
+        dateList.add(new SimpleDateFormat("yyyy-MM-dd").format(cal1.getTime()));
+        // 此日期是否在指定日期之后
+        while (cal2.getTime().after(cStart.getTime())) {
+            // 根据日历的规则，为给定的日历字段添加或减去指定的时间量
+            cStart.add(Calendar.DAY_OF_MONTH, 1);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            String dateStr = format.format(cStart.getTime());
+            dateList.add(dateStr);
+        }
+        //过滤周六 周日
+//      dateList = dateList.stream().limit(5).collect(Collectors.toList());
+        return dateList;
+    }
+
+    public static String toTimeString(long time) {
+        TimeUtils t = new TimeUtils(time);
+        int day = t.get(TimeUtils.DAY);
+        int hour = t.get(TimeUtils.HOUR);
+        int minute = t.get(TimeUtils.MINUTE);
+        int second = t.get(TimeUtils.SECOND);
+        StringBuilder sb = new StringBuilder();
+        if (day > 0) {
+            sb.append(day).append("天");
+        }
+        if (hour > 0) {
+            sb.append(hour).append("时");
+        }
+        if (minute > 0) {
+            sb.append(minute).append("分");
+        }
+        if (second > 0) {
+            sb.append(second).append("秒");
+        }
+        return sb.toString();
+    }
+
     /**
      * 时间字段常量，表示“秒”
      */
@@ -60,12 +104,12 @@ public class TimeUtils {
     /**
      * 各常量允许的最大值
      */
-    private final int[] maxFields = { 59, 59, 23, Integer.MAX_VALUE - 1 };
+    private final int[] maxFields = {59, 59, 23, Integer.MAX_VALUE - 1};
 
     /**
      * 各常量允许的最小值
      */
-    private final int[] minFields = { 0, 0, 0, Integer.MIN_VALUE };
+    private final int[] minFields = {0, 0, 0, Integer.MIN_VALUE};
 
     /**
      * 默认的字符串格式时间分隔符
@@ -86,8 +130,9 @@ public class TimeUtils {
 
     /**
      * 使用时、分构造一个时间
-     * @param hour      小时
-     * @param minute    分钟
+     *
+     * @param hour   小时
+     * @param minute 分钟
      */
     public TimeUtils(int hour, int minute) {
         this(0, hour, minute, 0);
@@ -95,9 +140,10 @@ public class TimeUtils {
 
     /**
      * 使用时、分、秒构造一个时间
-     * @param hour      小时
-     * @param minute    分钟
-     * @param second    秒
+     *
+     * @param hour   小时
+     * @param minute 分钟
+     * @param second 秒
      */
     public TimeUtils(int hour, int minute, int second) {
         this(0, hour, minute, second);
@@ -106,7 +152,8 @@ public class TimeUtils {
     /**
      * 使用一个字符串构造时间<br/>
      * Time time = new Time("14:22:23");
-     * @param time      字符串格式的时间，默认采用“:”作为分隔符
+     *
+     * @param time 字符串格式的时间，默认采用“:”作为分隔符
      */
     public TimeUtils(String time) {
         this(time, null);
@@ -115,26 +162,29 @@ public class TimeUtils {
 
     /**
      * 使用时间毫秒构建时间
+     *
      * @param time
      */
-    public TimeUtils(long time){
-    	this(new Date(time));
+    public TimeUtils(long time) {
+        this(new Date(time));
     }
-    
+
     /**
      * 使用日期对象构造时间
+     *
      * @param date
      */
-    public TimeUtils(Date date){
-    	this(DateFormatUtils.formatUTC(date, "HH:mm:ss"));
+    public TimeUtils(Date date) {
+        this(DateFormatUtils.formatUTC(date, "HH:mm:ss"));
     }
 
     /**
      * 使用天、时、分、秒构造时间，进行全字符的构造
-     * @param day       天
-     * @param hour      时
-     * @param minute    分
-     * @param second    秒
+     *
+     * @param day    天
+     * @param hour   时
+     * @param minute 分
+     * @param second 秒
      */
     public TimeUtils(int day, int hour, int minute, int second) {
         initialize(day, hour, minute, second);
@@ -143,10 +193,11 @@ public class TimeUtils {
     /**
      * 使用一个字符串构造时间，指定分隔符<br/>
      * Time time = new Time("14-22-23", "-");
-     * @param time      字符串格式的时间
+     *
+     * @param time 字符串格式的时间
      */
     public TimeUtils(String time, String timeSeparator) {
-        if(timeSeparator != null) {
+        if (timeSeparator != null) {
             setTimeSeparator(timeSeparator);
         }
         parseTime(time);
@@ -154,17 +205,18 @@ public class TimeUtils {
 
     /**
      * 设置时间字段的值
-     * @param field     时间字段常量
-     * @param value     时间字段的值
+     *
+     * @param field 时间字段常量
+     * @param value 时间字段的值
      */
     public void set(int field, int value) {
-        if(value < minFields[field]) {
+        if (value < minFields[field]) {
             throw new IllegalArgumentException(value + ", time value must be positive.");
         }
         fields[field] = value % (maxFields[field] + 1);
         // 进行进位计算
         int carry = value / (maxFields[field] + 1);
-        if(carry > 0) {
+        if (carry > 0) {
             int upFieldValue = get(field + 1);
             set(field + 1, upFieldValue + carry);
         }
@@ -172,11 +224,12 @@ public class TimeUtils {
 
     /**
      * 获得时间字段的值
-     * @param field     时间字段常量
-     * @return          该时间字段的值
+     *
+     * @param field 时间字段常量
+     * @return 该时间字段的值
      */
     public int get(int field) {
-        if(field < 0 || field > fields.length - 1) {
+        if (field < 0 || field > fields.length - 1) {
             throw new IllegalArgumentException(field + ", field value is error.");
         }
         return fields[field];
@@ -184,11 +237,12 @@ public class TimeUtils {
 
     /**
      * 将时间进行“加”运算，即加上一个时间
-     * @param time      需要加的时间
-     * @return          运算后的时间
+     *
+     * @param time 需要加的时间
+     * @return 运算后的时间
      */
     public TimeUtils addTime(TimeUtils time) {
-    	TimeUtils result = new TimeUtils();
+        TimeUtils result = new TimeUtils();
         int up = 0;     // 进位标志
         for (int i = 0; i < fields.length; i++) {
             int sum = fields[i] + time.fields[i] + up;
@@ -200,11 +254,12 @@ public class TimeUtils {
 
     /**
      * 将时间进行“减”运算，即减去一个时间
-     * @param time      需要减的时间
-     * @return          运算后的时间
+     *
+     * @param time 需要减的时间
+     * @return 运算后的时间
      */
     public TimeUtils subtractTime(TimeUtils time) {
-    	TimeUtils result = new TimeUtils();
+        TimeUtils result = new TimeUtils();
         int down = 0;       // 退位标志
         for (int i = 0, k = fields.length - 1; i < k; i++) {
             int difference = fields[i] + down;
@@ -223,6 +278,7 @@ public class TimeUtils {
 
     /**
      * 获得时间字段的分隔符
+     *
      * @return
      */
     public String getTimeSeparator() {
@@ -231,7 +287,8 @@ public class TimeUtils {
 
     /**
      * 设置时间字段的分隔符（用于字符串格式的时间）
-     * @param timeSeparator     分隔符字符串
+     *
+     * @param timeSeparator 分隔符字符串
      */
     public void setTimeSeparator(String timeSeparator) {
         this.timeSeparator = timeSeparator;
@@ -245,7 +302,7 @@ public class TimeUtils {
     }
 
     private void parseTime(String time) {
-        if(time == null) {
+        if (time == null) {
             initialize(0, 0, 0, 0);
             return;
         }
@@ -253,7 +310,7 @@ public class TimeUtils {
         int field = DAY;
         set(field--, 0);
         int p = -1;
-        while((p = t.indexOf(timeSeparator)) > -1) {
+        while ((p = t.indexOf(timeSeparator)) > -1) {
             parseTimeField(time, t.substring(0, p), field--);
             t = t.substring(p + timeSeparator.length());
         }
@@ -261,16 +318,16 @@ public class TimeUtils {
     }
 
     private void parseTimeField(String time, String t, int field) {
-        if(field < SECOND || t.length() < 1) {
+        if (field < SECOND || t.length() < 1) {
             parseTimeException(time);
         }
         char[] chs = t.toCharArray();
         int n = 0;
-        for(int i = 0; i < chs.length; i++) {
-            if(chs[i] <= ' ') {
+        for (int i = 0; i < chs.length; i++) {
+            if (chs[i] <= ' ') {
                 continue;
             }
-            if(chs[i] >= '0' && chs[i] <= '9') {
+            if (chs[i] >= '0' && chs[i] <= '9') {
                 n = n * 10 + chs[i] - '0';
                 continue;
             }
@@ -294,7 +351,7 @@ public class TimeUtils {
     }
 
     private StringBuilder buildString(StringBuilder sb, int field) {
-        if(fields[field] < 10) {
+        if (fields[field] < 10) {
             sb.append('0');
         }
         return sb.append(fields[field]);
@@ -320,5 +377,5 @@ public class TimeUtils {
         }
         return true;
     }
-    
+
 }

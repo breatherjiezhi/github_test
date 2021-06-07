2e604b07dc2eb399e39d5b8d9e1db6070b5275b2
songtangwen <15161487238@dingtalk.com>
2021-06-07 11:26:45 +0800

    1.增加配置文件查询接口

--- src/main/java/com/dhc/rad/modules/wx/controller/WxConfigInfoController.java
+++ src/main/java/com/dhc/rad/modules/wx/controller/WxConfigInfoController.java
@@ -1,8 +1,8 @@
 package com.dhc.rad.modules.wx.controller;
 
 
-import com.dhc.rad.common.config.Global;
 import com.dhc.rad.common.utils.ConstantUtils;
+import com.dhc.rad.common.web.BaseController;
 import com.dhc.rad.modules.sys.entity.ConfigInfo;
 import com.dhc.rad.modules.sys.service.ConfigInfoService;
 import com.dhc.rad.modules.sys.utils.ConfigInfoUtils;
@@ -14,12 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping(value = "${adminPath}/wx/wxConfigInfo")
-public class WxConfigInfoController {
+@RequestMapping(value = "${adminPath}/wx/configInfo")
+public class WxConfigInfoController extends BaseController {
 
 
     @Autowired
@@ -28,15 +29,19 @@ public class WxConfigInfoController {
     /*
      * wx帮助与建议
      */
-    @RequestMapping(value = "getConfigInfo")
+    @RequestMapping(value = "getHelpInfo")
     @ResponseBody
-    public Map<String, Object> getConfigInfo(RedirectAttributes redirectAttributes) {
+    public Map<String, Object> getHelpInfo(RedirectAttributes redirectAttributes) {
         Map<String, Object> returnMap = Maps.newHashMap();
-        ConfigInfo c1 = configInfoService.get("helpInfo");
-        ConfigInfo c2 = configInfoService.get("rollInfo");
-        List<ConfigInfo> dataList = new ArrayList<ConfigInfo>();
-        dataList.add(c1);
-        dataList.add(c2);
+
+        List<Map<String,Object>> dataList= new ArrayList<>();
+        Map<String,Object> map = new HashMap<>();
+
+        String helpInfo = ConfigInfoUtils.getConfigVal("helpInfo");
+        map.put("helpInfo",helpInfo);
+        String rollInfo = ConfigInfoUtils.getConfigVal("rollInfo");
+        map.put("rollInfo",rollInfo);
+        dataList.add(map);
         returnMap.put("data", dataList);
         returnMap.put("status", ConstantUtils.ResCode.SUCCESS);
         returnMap.put("message", ConstantUtils.ResCode.SUCCESSMSG);
